46d4839f3bcb50345a85a7f45f27b098d76de38c
sojeongLee19 <55444000+sojeongLee19@users.noreply.github.com>
2019-11-07 15:25:25 +0900

    03

--- main.c
+++ main.c
@@ -3,19 +3,10 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-    int i=10;
-    char c='a';	
-    
-    int *iptr;
-    iptr= &i;
-    char *cptr;
-    cptr= &c;
-    int *iptr2=iptr;
+void main(void) { 
+int i = 300; 
 
- 	printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr)); 
-	printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr)); 
-	printf("iptr2 : %p, %i\n", iptr2, *iptr2); 
-	 	
-	return 0;
+int *pi = &i; 
+char *pc = &i; 
+printf("%i, %i, %i\n", i, *pi, *pc); 
 }
